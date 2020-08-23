e07076ed4707c1c477979d9898fe1b064723c1d9
Zach Dahir <zachdahir@yahoo.com>
2020-08-23 13:32:00 -0500

    Adding week 7

---
+++ week-7/gpa-calculator-app3/src/app/sign-in.service.ts
@@ -0,0 +1,24 @@
+/**
+ * Title: 7.2
+ * Author: Zachary Dahir
+ * Date: 8-17-20
+ * Description: sign in service
+ */
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignInService {
+
+  studentIds: Array<number>;
+
+  constructor() {
+    this.studentIds = [1007, 1008, 1009, 1010, 1011, 1012];
+   }
+
+   validate(studentId: number) {
+     return this.studentIds.some(id => id === studentId);
+   }
+}
